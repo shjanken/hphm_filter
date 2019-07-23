d92c24b82766e1f09294e26f68d762e252ddaca7
shjanken <janken.wang@hotmail.com>
2019-07-23 14:51:26 +0800

    统一代码风格，添加了注释

--- hphm_filter.pl
+++ hphm_filter.pl
@@ -1,16 +1,16 @@
 #!/usr/bin/env perl
 
-use v5.30;
+use v5.20;
 use warnings;
 use utf8;
 
 my $debug = 0;
 
-my $content = $ARGV[0];
+my $content = $ARGV[0]; # 获取启动参数
 
 # filter the hphm from content string.
 my @matcheds = $content =~ m/[a-z]{1}[a-z0-9]{5,6}/ig;
-say "@matcheds" if ($debug);
+say "@matcheds" if $debug;
 
 # send the hphm to clipboard
 foreach my $matched (@matcheds) {
